040c4178da1c30527b0166389e1556efbbf6d277
adrixo <adrianvalrom@gmail.com>
2022-10-17 14:21:36 +0200

    feat: sum simple game result with only numbers

--- src/gameparser.go
+++ src/gameparser.go
@@ -1,5 +1,17 @@
 package src
 
+import (
+	"strconv"
+	"strings"
+)
+
 func parse(gameResult string) (result int) {
-	return
+	frames := strings.Split(gameResult, "|")
+	for _, frame := range frames {
+		for _, symbol := range frame {
+			number, _ := strconv.Atoi(string(symbol))
+			result += number
+		}
+	}
+	return result
 }
