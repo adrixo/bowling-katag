b30c50ce7b5be7ee367d7833141fa2ce6a8e6353
adrixo <adrianvalrom@gmail.com>
2022-10-17 14:43:50 +0200

    feat: ignore miss simbol

--- src/gameparser.go
+++ src/gameparser.go
@@ -8,9 +8,22 @@ import (
 func parse(gameResult string) (result int) {
 	frames := strings.Split(gameResult, "|")
 	for _, frame := range frames {
-		firstThrow, _ := strconv.Atoi(string(frame[0]))
-		secondThrow, _ := strconv.Atoi(string(frame[1]))
+		firstSymbol := string(frame[0])
+		var firstThrow int
+		if firstSymbol == "-" {
+			firstThrow = 0
+		} else {
+			firstThrow, _ = strconv.Atoi(firstSymbol)
+		}
 		result += firstThrow
+
+		secondSymbol := string(frame[1])
+		var secondThrow int
+		if secondSymbol == "-" {
+			secondThrow = 0
+		} else {
+			secondThrow, _ = strconv.Atoi(secondSymbol)
+		}
 		result += secondThrow
 	}
 	return result
