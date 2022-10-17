f268ec84a5a01f548c0ef4421887e9c119547ef8
adrixo <adrianvalrom@gmail.com>
2022-10-17 14:16:37 +0200

    test(red): parse numeral frames

--- src/gameparser.go
+++ src/gameparser.go
@@ -1,18 +1,5 @@
 package src
 
-import "strconv"
-
 func parse(gameResult string) (result int) {
 	return
-}
-
-func parseFrame(input string) (result int, e error) {
-	for _, s := range input {
-		if string(s) == "-" {
-			continue
-		}
-		number, _ := strconv.Atoi(string(s))
-		result += number
-	}
-	return result, e
 }
--- src/gameparser_test.go
+++ src/gameparser_test.go
@@ -1,40 +1,12 @@
 package src
 
-import (
-	"testing"
-)
+import "testing"
 
-func Test_parse_number_frame(t *testing.T) {
-	numbers := []string{"18", "9", "27", "36", "45"}
-	expecteds := []int{9, 9, 9, 9, 9}
-	for i, number := range numbers {
-		expected := expecteds[i]
-		result, _ := parseFrame(number)
-		if expected != result {
-			t.Errorf("Expected %v; but got %v", expected, result)
-		}
-	}
-}
-
-func Test_parse_miss_frame(t *testing.T) {
-	numbers := []string{"1-", "5-", "--", "-"}
-	expecteds := []int{1, 5, 0, 0}
-	for i, number := range numbers {
-		expected := expecteds[i]
-		result, _ := parseFrame(number)
-		if expected != result {
-			t.Errorf("Expected %v; but got %v", expected, result)
-		}
-	}
-}
-
-func Test_spare_frame(t *testing.T) {
-	numbers := []string{"1/", "5/", "-/", "9/"}
-	for _, number := range numbers {
-		expected := 10
-		result, _ := parseFrame(number)
-		if expected != result {
-			t.Errorf("Expected %v; but got %v", expected, result)
-		}
+func Test_first(t *testing.T) {
+	number := "23|42|32"
+	expected := 16
+	result := parse(number)
+	if expected != result {
+		t.Errorf("Expected %v; but got %v", expected, result)
 	}
 }
