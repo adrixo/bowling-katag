04da2d0be5372a9780d61e8c984060c568c93ed6
adrixo <adrianvalrom@gmail.com>
2022-10-17 15:10:29 +0200

    feat: give 10 points on spare

--- src/gameparser.go
+++ src/gameparser.go
@@ -8,19 +8,23 @@ import (
 func parse(gameResult string) (result int) {
 	frames := strings.Split(gameResult, "|")
 	for _, frame := range frames {
-		result += parseSymbol(string(frame[0]))
-		result += parseSymbol(string(frame[1]))
+		frameResult := 0
+		for _, symbol := range frame {
+			if string(symbol) == "-" {
+				continue
+			}
+			if string(symbol) == "X" {
+				frameResult = 10
+				continue
+			}
+			if string(symbol) == "/" {
+				frameResult = 10
+				continue
+			}
+			value, _ := strconv.Atoi(string(symbol))
+			frameResult += value
+		}
+		result += frameResult
 	}
-	return result
-}
-
-func parseSymbol(symbol string) (result int) {
-	if symbol == "-" {
-		return 0
-	}
-	if symbol == "X" {
-		return 10
-	}
-	result, _ = strconv.Atoi(symbol)
 	return result
 }
--- src/gameparser_test.go
+++ src/gameparser_test.go
@@ -22,7 +22,16 @@ func Test_should_ignore_miss_symbol(t *testing.T) {
 
 func Test_should_give_10_points_on_strike(t *testing.T) {
 	number := "X|--|X|--"
-	expected := 10
+	expected := 20
+	result := parse(number)
+	if expected != result {
+		t.Errorf("Expected %v; but got %v", expected, result)
+	}
+}
+
+func Test_should_give_10_points_on_spare(t *testing.T) {
+	number := "4/|--|7/|--"
+	expected := 20
 	result := parse(number)
 	if expected != result {
 		t.Errorf("Expected %v; but got %v", expected, result)
